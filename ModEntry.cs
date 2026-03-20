5e6f0bb533d0e620ad5604f4f169395b046d1cee
dragonnnii <159734819+dragonnnii@users.noreply.github.com>
2026-03-20 16:36:49 +0300

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,21 +1,44 @@
-﻿using Harmony;
-using Microsoft.Xna.Framework.Input;
+using System;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
-using System.Reflection;
 
-namespace LeftyMode
+namespace LeftClickToRightClick
 {
     public class ModEntry : Mod
     {
+        private bool isRightClickEnabled = false;
+
         public override void Entry(IModHelper helper)
         {
-            var harmony = HarmonyInstance.Create(this.ModManifest.UniqueID);
-            harmony.Patch(
-               original: AccessTools.Method(typeof(Mouse), nameof(Mouse.GetState)),
-               postfix: new HarmonyMethod(typeof(LeftyMouse), nameof(LeftyMouse.GetState))
-            );
+            // Listen for key presses and mouse/touch input
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
+        }
+
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            // Toggle mode when 'W' is pressed
+            if (e.Button == SButton.W)
+            {
+                isRightClickEnabled = !isRightClickEnabled;
+                string status = isRightClickEnabled ? "ENABLED" : "DISABLED";
+                
+                // Show a message on screen so you know it worked
+                Game1.addHUDMessage(new HUDMessage($"Right-Click Mode: {status}", 3));
+
+                // Prevent the 'W' from making the character walk up while toggling
+                this.Helper.Input.Suppress(SButton.W);
+            }
+
+            // If mode is ON and user taps (MouseLeft), swap it to MouseRight
+            if (isRightClickEnabled && e.Button == SButton.MouseLeft)
+            {
+                // Stop the normal Left-Click
+                this.Helper.Input.Suppress(SButton.MouseLeft);
+
+                // Force a Right-Click at the current cursor position
+                this.Helper.Input.TriggerMouseButton(SButton.MouseRight);
+            }
         }
     }
 }
