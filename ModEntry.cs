f907a5cf0e40171f0bda571d50424010a8c0df3c
dragonnnii <159734819+dragonnnii@users.noreply.github.com>
2026-03-22 00:39:15 +0300

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -10,42 +10,30 @@ namespace LeftyMode
 {
     public class ModEntry : Mod
     {
-        // Tracks if we are in normal mode (true) or lefty/right-click mode (false)
         private bool IsLeftClickMode = true;
 
         public override void Entry(IModHelper helper)
         {
-            // Set up the listener for button presses (like the 'W' key)
             helper.Events.Input.ButtonsChanged += this.OnButtonsChanged;
         }
 
         private void OnButtonsChanged(object sender, ButtonsChangedEventArgs e)
         {
-            // 1. Toggle Mode Logic (Press 'W' to switch)
-            if (e.Pressed.Contains(SButton.W))
+            // Changed from SButton.W to SButton.RightStick (D3)
+            if (e.Pressed.Contains(SButton.RightStick))
             {
                 this.IsLeftClickMode = !this.IsLeftClickMode;
                 string modeName = this.IsLeftClickMode ? "Left Click" : "Right Click";
 
-                // Show a message on the screen in-game
-                StardewValley.Game1.addHUDMessage(new StardewValley.HUDMessage($"Mouse Mode: {modeName}", 3));
-                
-                // Log the change to the SMAPI console
-                this.Monitor.Log($"Switched to {modeName} mode.", LogLevel.Info);
+                Game1.addHUDMessage(new HUDMessage($"Mouse Mode: {modeName}", 3));
+                this.Monitor.Log($"Switched to {modeName} mode using D3.", LogLevel.Info);
             }
 
-            // 2. Mouse Input Logic
-            // This part detects physical mouse clicks using MonoGame (Microsoft.Xna.Framework.Input)
+            // Mouse logic remains the same
             MouseState mouseState = Mouse.GetState();
-
-            if (mouseState.LeftButton == ButtonState.Pressed)
+            if (mouseState.LeftButton == ButtonState.Pressed && !this.IsLeftClickMode)
             {
-                if (!this.IsLeftClickMode)
-                {
-                    // If you are in 'Right Click' mode, we can add logic here 
-                    // to trigger game actions usually reserved for the right mouse button.
-                    this.Monitor.Log("Left click detected while in Right-Click Mode!", LogLevel.Debug);
-                }
+                this.Monitor.Log("D3 Mode: Simulating Right Click...", LogLevel.Debug);
             }
         }
     }
