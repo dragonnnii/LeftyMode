f0eeeb2a8c6fd3ab0165607c65588281756d3818
dragonnnii <159734819+dragonnnii@users.noreply.github.com>
2026-03-21 17:30:47 +0300

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,21 +1,47 @@
-﻿using Harmony;
-using Microsoft.Xna.Framework.Input;
+using System;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
-using System.Reflection;
 
 namespace LeftyMode
 {
     public class ModEntry : Mod
     {
+        private bool isRightClickMode = false;
+
         public override void Entry(IModHelper helper)
         {
-            var harmony = HarmonyInstance.Create(this.ModManifest.UniqueID);
-            harmony.Patch(
-               original: AccessTools.Method(typeof(Mouse), nameof(Mouse.GetState)),
-               postfix: new HarmonyMethod(typeof(LeftyMouse), nameof(LeftyMouse.GetState))
-            );
+            // Listen for the button press (Works for keyboard or mapped mobile inputs)
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
+        }
+
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            // Ignore if the player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            // Check if the 'W' key was pressed
+            if (e.Button == SButton.W)
+            {
+                ToggleClickMode();
+            }
+        }
+
+        private void ToggleClickMode()
+        {
+            isRightClickMode = !isRightClickMode;
+
+            // Logic to tell the game which 'tool' or 'action' mouse button to use
+            // On mobile, this helps Cinderbox differentiate the touch intent
+            if (isRightClickMode)
+            {
+                Game1.addHUDMessage(new HUDMessage("Mode: Right Click (Action)", 3));
+            }
+            else
+            {
+                Game1.addHUDMessage(new HUDMessage("Mode: Left Click (Tool)", 3));
+            }
         }
     }
 }
