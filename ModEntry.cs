6f4071d942ae38a14f708dac9faa37191636c54c
dragonnnii <159734819+dragonnnii@users.noreply.github.com>
2026-03-22 00:26:42 +0300

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
-using StardewValley;
+using StardewValley; // <--- THIS LINE IS REQUIRED FOR Game1
 
 namespace LeftyMode
 {
@@ -13,25 +13,20 @@ namespace LeftyMode
 
         public override void Entry(IModHelper helper)
         {
-            // The name here...
             helper.Events.Input.ButtonsChanged += OnButtonsChanged;
         }
 
-        // ...MUST match the name here!
         private void OnButtonsChanged(object sender, ButtonsChangedEventArgs e)
         {
             if (e.Pressed.Contains(SButton.W))
             {
                 IsLeftClickMode = !IsLeftClickMode;
                 string mode = IsLeftClickMode ? "Left Click" : "Right Click";
+                
+                // We use StardewValley.Game1 to be 100% sure the compiler finds it
+                StardewValley.Game1.addHUDMessage(new StardewValley.HUDMessage($"Mouse Mode: {mode}", 3));
+                
                 this.Monitor.Log($"Switched to {mode} mode.", LogLevel.Info);
-            }
-
-            // Simple Mouse Check
-            MouseState mouseState = Mouse.GetState();
-            if (mouseState.LeftButton == ButtonState.Pressed && !IsLeftClickMode)
-            {
-                // Logic for right-click simulation
             }
         }
     }
