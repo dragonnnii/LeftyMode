acda6e5c7f20af4be54d7ed766cb71813d227653
dragonnnii <159734819+dragonnnii@users.noreply.github.com>
2026-03-22 00:36:29 +0300

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -10,24 +10,42 @@ namespace LeftyMode
 {
     public class ModEntry : Mod
     {
+        // Tracks if we are in normal mode (true) or lefty/right-click mode (false)
         private bool IsLeftClickMode = true;
 
         public override void Entry(IModHelper helper)
         {
+            // Set up the listener for button presses (like the 'W' key)
             helper.Events.Input.ButtonsChanged += this.OnButtonsChanged;
         }
 
         private void OnButtonsChanged(object sender, ButtonsChangedEventArgs e)
         {
+            // 1. Toggle Mode Logic (Press 'W' to switch)
             if (e.Pressed.Contains(SButton.W))
             {
                 this.IsLeftClickMode = !this.IsLeftClickMode;
                 string modeName = this.IsLeftClickMode ? "Left Click" : "Right Click";
 
-                // Using the full path ensures the compiler finds Game1 and HUDMessage
+                // Show a message on the screen in-game
                 StardewValley.Game1.addHUDMessage(new StardewValley.HUDMessage($"Mouse Mode: {modeName}", 3));
                 
+                // Log the change to the SMAPI console
                 this.Monitor.Log($"Switched to {modeName} mode.", LogLevel.Info);
+            }
+
+            // 2. Mouse Input Logic
+            // This part detects physical mouse clicks using MonoGame (Microsoft.Xna.Framework.Input)
+            MouseState mouseState = Mouse.GetState();
+
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                if (!this.IsLeftClickMode)
+                {
+                    // If you are in 'Right Click' mode, we can add logic here 
+                    // to trigger game actions usually reserved for the right mouse button.
+                    this.Monitor.Log("Left click detected while in Right-Click Mode!", LogLevel.Debug);
+                }
             }
         }
     }
